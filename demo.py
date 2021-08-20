50a5871290ee799f4cd7a4d0d6c1c2edbb5320a2
athelind <edge.of.time@gmail.com>
2021-08-20 20:19:12 +0800

    Demobnnb

--- demo.py
+++ demo.py
@@ -11,7 +11,7 @@ class Visitors():
 
     def __init__(self):
         xls = pd.ExcelFile("Project_File.xlsx")
-        print(xls)
+        # print(xls)
         self.myCols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
         self.myColNames = ["MonthYear", "Brunei Darussalam", "Indonesia", "Malaysia", "Philippines", "Thailand", "Viet Nam", "Myanmar", "Japan", "Hong Kong", "China", "Taiwan", "Korea, Republic Of"]
         self.mydata = xls.parse(0, usecols= self.myCols, names = self.myColNames)
@@ -26,7 +26,7 @@ class Visitors():
 
         self.mydata = self.mydata.assign(Month=splitYear[1])
         # print("\nAdded back Month Column")
-        #print(self.mydata)
+        # print(self.mydata)
 
         self.mydata = self.mydata[self.mydata["Year"].astype(int) >= 2000]
         # print("\nRemove Years less than 2000")
@@ -34,20 +34,18 @@ class Visitors():
 
         self.mydata = self.mydata[self.mydata["Year"].astype(int) <= 2001]
         # print("\nRemove Years more than 2001")
-        #print(self.mydata)
+        print(self.mydata)
 
     def generate2000MalaysiaAvg(self):
-        print("generating MY avg")
+        # print("generating MY avg")
         # print(self.mydata)
 
-        print(self.mydata.iloc[:, [3,13,14]] )
+        # print("\nGet the MY columns")
+        # print(self.mydata.iloc[:, [3,13,14]] )
         malaysia = self.mydata.iloc[:, [3,13,14]]
-
-
         malaysia2000 = malaysia[malaysia["Year"] == "2000"]
-        print("\nOnly MY 2000")
-        print(malaysia2000)
-
+        # print("\nOnly MY 2000")
+        # print(malaysia2000)
         myMean = malaysia2000["Malaysia"].mean()
         print("\nThe average visitors from Malaysia in 2000: " + str(myMean))
 
@@ -61,11 +59,10 @@ class Visitors():
         plt.plot(malaysia2000["Month"], malaysia2000["Malaysia"])
         plt.title("Malaysian Visitors in Year 2000")
         plt.show()
-
+#dgfbfdbfgbf
     def getMalaysia2000VisitorCount(self):
         malaysia = self.mydata.iloc[:, [3,13,14]]
-        malaysia2000 = malaysia[malaysia["Year"] == "2000"]
-
+        malaysia2000 = malaysia[malaysia["Year"]== "2000" ]
         return malaysia2000["Malaysia"].sum()
 
     def getTop3Countries(self):
@@ -93,7 +90,7 @@ class Visitors():
         print(self.thirdTopVisitorCnt)
 
 # test = Visitors()
-# #test.generate2000MalaysiaAvg()
+# test.generate2000MalaysiaAvg()
 # test.makeMalaysia2000LineChart()
 # test.getTop3Countries()
 # print(test.getMalaysia2000VisitorCount())
--- visitortest.py
+++ visitortest.py
@@ -6,4 +6,4 @@ class testMath(unittest.TestCase):
     def testMYVisitors(self):
         visitor = Visitors()
         result = Visitors.getMalaysia2000VisitorCount(visitor)
-        self.assertEqual(result, 564752)
+        self.assertEqual(result, 564790)
